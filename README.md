a37a50ad0ec7323f6af53126db40cab664e15d10
TrungDT <thanhtrung.it25@gmail.com>
2019-05-21 15:20:12 +0700

    update README.md

---
+++ README.md
@@ -0,0 +1,14 @@
+# Issue Tracker (Node, React)
+
+## Features of this project
+- Express REST APIs
+- MongoDB
+- Modularization and webpack
+- Routing with React Router
+- Forms
+- React bootstrap
+- Server rendering
+- Pagination
+- Search Bar
+- Google sign-in
+- Session
---
+++ static/sample-config.js
@@ -0,0 +1,9 @@
+window.config = {
+  googleClientId: 'YOUR-GOOGLE-CLIENT-ID',
+};
+
+// https://developers.google.com/identity/sign-in/web/
+// https://console.developers.google.com/apis/credentials
+  // OAuth 2.0 client IDs
+  // Authorized JavaScript origins
+  // http://localhost:8888
